a01b0f0d569f3b460e2560aa6574ee8b253a45bd
Joel <joeldome17@gmail.com>
2022-12-20 01:47:13 -0600

    Fusión del menú en mobile

--- curso_practico_javascript/js/index.js
+++ curso_practico_javascript/js/index.js
@@ -1,8 +1,15 @@
 $navbar__account = document.querySelector('.navbar__account > p')
 $menu_card = document.querySelector('.menu__card')
+$hamburgerIcon = document.querySelector('.navbar__icon')
+$menuMobile = document.querySelector('.menu')
 
 const toggleDesktopMenu = () =>{
   $menu_card.classList.toggle('inactive')
 }
 
+const toggleMobileMenu = () => {
+  $menuMobile.classList.toggle('inactive')
+}
+
 $navbar__account.addEventListener('click', toggleDesktopMenu)
+$hamburgerIcon.addEventListener('click', toggleMobileMenu)
